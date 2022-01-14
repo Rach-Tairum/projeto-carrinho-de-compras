20fca9b2ec4814104273013f5e80aa5bd455bb7a
Rachel Tairum <rachel.tairum@gmail.com>
2022-01-14 19:23:44 -0300

    Requisito 9

--- helpers/fetchItem.js
+++ helpers/fetchItem.js
@@ -1,5 +1,12 @@
-const fetchItem = () => {
-  // seu código aqui
+const fetchItem = async (identificador) => {
+  const urlUtilizado = `https://api.mercadolibre.com/items/${identificador}`;
+  try {
+    const resposta = await fetch(urlUtilizado);
+    const dadosProduto = await resposta.json();
+    return dadosProduto;
+  } catch (error) {
+      return error;
+  }
 };
 
 if (typeof module !== 'undefined') {
